4af830ffe16129244be36e5001510fa804f4cd91
Ashutosh Singh <singh.saahab08@gmail.com>
2024-01-17 16:03:02 +0530

    complete frontend with new package json changes

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -68,7 +68,7 @@ const ProductScreen = ({ history, match }) => {
 
   return (
     <>
-      <Link className="btn btn-light my-3" to="/">
+      <Link className="btn btn-light my-3" to="/home">
         Go Back
       </Link>
       {loading ? (
