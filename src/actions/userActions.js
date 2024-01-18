33afb46b0f25b83e03bf1759e5c4b26093394279
Ashutosh Singh <75207669+cybermonk01@users.noreply.github.com>
2024-01-18 11:06:25 +0530

    Update userActions.js

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -40,7 +40,7 @@ export const login = (email, password) => async (dispatch) => {
     };
 
     const { data } = await axios.post(
-      "/api/users/login",
+      "https://ak-api.vercel.app/api/users/login",
       { email, password },
       config
     );
